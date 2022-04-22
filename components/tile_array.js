54dfc76a1c85921484798d10ecc8ccc0c06c932c
Delaine Abner <50344531+delaine3@users.noreply.github.com>
2022-04-22 16:59:23 -0400

    Updated images

--- components/tile_array.js
+++ components/tile_array.js
@@ -1,82 +1,82 @@
 export const tileArray = [
     {
-      name: 'Panda',
-      img: 'https://sweetclipart.com/wp-content/uploads/Cute-Panda-Bear-Clip-Art-2.png',
+      name: 'Ocean',
+      img: 'https://images.unsplash.com/photo-1583212292454-1fe6229603b7?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxzZWFyY2h8MTJ8fGZpc2h8ZW58MHx8MHx8&auto=format&fit=crop&w=500&q=60',
       id : 1
     },
     {
-      name: 'Panda',
-      img: 'https://sweetclipart.com/wp-content/uploads/Cute-Panda-Bear-Clip-Art-2.png',
+      name: 'Ocean',
+      img: 'https://images.unsplash.com/photo-1583212292454-1fe6229603b7?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxzZWFyY2h8MTJ8fGZpc2h8ZW58MHx8MHx8&auto=format&fit=crop&w=500&q=60',
       id : 2
     },
     {
-      name: 'Cactus',
-      img: 'https://tse2.mm.bing.net/th?id=OIP.FBwdbj7kmD3jPaq-OorzxAHaN5&pid=Api',
+      name: 'Balloons',
+      img: 'https://images.unsplash.com/photo-1509909756405-be0199881695?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxzZWFyY2h8NHx8aGFwcHl8ZW58MHx8MHx8&auto=format&fit=crop&w=500&q=60',
       id : 3
     },
     {
-      name: 'Cactus',
-      img: 'https://tse2.mm.bing.net/th?id=OIP.FBwdbj7kmD3jPaq-OorzxAHaN5&pid=Api',
+      name: 'Balloons',
+      img: 'https://images.unsplash.com/photo-1509909756405-be0199881695?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxzZWFyY2h8NHx8aGFwcHl8ZW58MHx8MHx8&auto=format&fit=crop&w=500&q=60',
       id : 4
     },
     {
       name: 'Koala',
-      img: 'https://sweetclipart.com/multisite/sweetclipart/files/koala_on_tree.png',
+      img: 'https://images.unsplash.com/photo-1579649554660-463ed1d72831?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxzZWFyY2h8NHx8a29hbGF8ZW58MHx8MHx8&auto=format&fit=crop&w=500&q=60',
       id : 5
     },
     {
       name: 'Koala',
-      img: 'https://sweetclipart.com/multisite/sweetclipart/files/koala_on_tree.png',
+      img: 'https://images.unsplash.com/photo-1579649554660-463ed1d72831?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxzZWFyY2h8NHx8a29hbGF8ZW58MHx8MHx8&auto=format&fit=crop&w=500&q=60',
       id : 6
     },
     {
       name: 'Bird',
-      img: 'https://sweetclipart.com/multisite/sweetclipart/files/spring_bird_3_red.png',
+      img: 'https://images.unsplash.com/photo-1552728089-57bdde30beb3?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxzZWFyY2h8NHx8YmlyZHxlbnwwfHwwfHw%3D&auto=format&fit=crop&w=500&q=60',
       id : 7
     },
     {
       name: 'Bird',
-      img: 'https://sweetclipart.com/multisite/sweetclipart/files/spring_bird_3_red.png',
+      img: 'https://images.unsplash.com/photo-1552728089-57bdde30beb3?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxzZWFyY2h8NHx8YmlyZHxlbnwwfHwwfHw%3D&auto=format&fit=crop&w=500&q=60',
       id : 8
     },
     {
       name: 'Sunflower',
-      img: 'https://sweetclipart.com/multisite/sweetclipart/files/sunflower_2.png',
+      img: 'https://images.unsplash.com/photo-1470509037663-253afd7f0f51?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxzZWFyY2h8NDh8fGhhcHB5fGVufDB8fDB8fA%3D%3D&auto=format&fit=crop&w=500&q=60',
       id : 9
     },
     {
       name: 'Sunflower',
-      img: 'https://sweetclipart.com/multisite/sweetclipart/files/sunflower_2.png',
+      img: 'https://images.unsplash.com/photo-1470509037663-253afd7f0f51?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxzZWFyY2h8NDh8fGhhcHB5fGVufDB8fDB8fA%3D%3D&auto=format&fit=crop&w=500&q=60',
       id : 10
     },
     {
       name: 'Volcano',
-      img: 'https://sweetclipart.com/wp-content/uploads/Volcano-Clip-Art-500x355.png',
+      img: 'https://images.unsplash.com/photo-1628632577468-645afac21c82?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxzZWFyY2h8MTF8fHZvbGNhbm98ZW58MHx8MHx8&auto=format&fit=crop&w=500&q=60',
       id : 11
     },
     {
       name: 'Volcano',
-      img: 'https://sweetclipart.com/wp-content/uploads/Volcano-Clip-Art-500x355.png',
+      img: 'https://images.unsplash.com/photo-1628632577468-645afac21c82?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxzZWFyY2h8MTF8fHZvbGNhbm98ZW58MHx8MHx8&auto=format&fit=crop&w=500&q=60',
       id : 12
     },
     {
       name: 'Planet',
-      img: 'https://sweetclipart.com/wp-content/uploads/Alien-Ringed-Planet-Pastel-Clip-Art.png',
+      img: 'https://images.unsplash.com/photo-1614732414444-096e5f1122d5?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxzZWFyY2h8MXx8cGxhbmV0fGVufDB8fDB8fA%3D%3D&auto=format&fit=crop&w=500&q=60',
       id : 13
     },
     {
       name: 'Planet',
-      img: 'https://sweetclipart.com/wp-content/uploads/Alien-Ringed-Planet-Pastel-Clip-Art.png',
+      img: 'https://images.unsplash.com/photo-1614732414444-096e5f1122d5?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxzZWFyY2h8MXx8cGxhbmV0fGVufDB8fDB8fA%3D%3D&auto=format&fit=crop&w=500&q=60',
       id : 14
     },
       {
         name: 'Lion',
-        img: 'https://sweetclipart.com/wp-content/uploads/Lion-Cute-Clip-Art-500x492.png',
+        img: 'https://images.unsplash.com/photo-1534188753412-3e26d0d618d6?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxzZWFyY2h8OXx8bGlvbnxlbnwwfHwwfHw%3D&auto=format&fit=crop&w=500&q=60',
         id : 15
       },
       {
         name: 'Lion',
-        img: 'https://sweetclipart.com/wp-content/uploads/Lion-Cute-Clip-Art-500x492.png',
+        img: 'https://images.unsplash.com/photo-1534188753412-3e26d0d618d6?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxzZWFyY2h8OXx8bGlvbnxlbnwwfHwwfHw%3D&auto=format&fit=crop&w=500&q=60',
         id : 16
       }
   ]
